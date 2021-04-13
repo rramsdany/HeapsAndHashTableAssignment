9c472d5a129700a28d3e474dfdfa9d1fbdebcbad
rramsdany <58965151+rramsdany@users.noreply.github.com>
2021-04-12 21:40:03 -0700

    Added AppDriver

---
+++ Java/src/AppDriver.java
@@ -0,0 +1,14 @@
+import java.util.PriorityQueue;
+public class AppDriver {
+
+    public static void main(String[] args) {
+        PriorityQueue<String> myGuess = new PriorityQueue<String>();
+        myGuess.add("pizza");
+        myGuess.add("cookie");
+        myGuess.add("ramen");
+        myGuess.add("chips");
+
+
+
+    }
+}
